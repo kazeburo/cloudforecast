7f4a2c4408a30b93908ce788dce7c7d4f80abb21
Masahiro Nagano <kazeburo@gmail.com>
2010-05-20 17:18:47 +0900

    webのリファクタ

--- lib/CloudForecast/Web.pm
+++ lib/CloudForecast/Web.pm
@@ -3,20 +3,25 @@ package CloudForecast::Web;
 use strict;
 use warnings;
 use Carp qw//;
+use Encode qw//;
 use Scalar::Util qw/refaddr/;
-use Plack::Runner;
+use base qw/Class::Data::Inheritable Class::Accessor::Fast/;
+use Plack::Loader;
+use Plack::Builder;
 use Plack::Request;
 use Plack::Response;
 use Router::Simple;
 use Text::MicroTemplate;
 use Data::Section::Simple;
+use CloudForecast::Log;
+use CloudForecast::ConfigLoader;
 
-my $_ROUTER;
-my %CACHE;
-our $KEY;
-our $DATA_SECTION_LEVEL = 0;
+__PACKAGE__->mk_classdata('_ROUTER');
+__PACKAGE__->mk_accessors(qw/configloader
+                             restarter
+                             port/);
 
-our @EXPORT = qw/get post any render run_server/;
+our @EXPORT = qw/get post any/;
 
 sub import {
     my ($class, $name) = @_;
@@ -24,12 +29,12 @@ sub import {
     {
         no strict 'refs';
         if ( $name && $name =~ /^-base/ ) {
-
-            $_ROUTER = Router::Simple->new();
-
-            for my $func (@EXPORT) {
-                *{"$caller\::$func"} = \&$func;
+            if ( ! $caller->isa($class) && $caller ne 'main' ) {
+                push @{"$caller\::ISA"}, $class;
             }
+        }
+        for my $func (@EXPORT) {
+            *{"$caller\::$func"} = \&$func;
         }
     }
 
@@ -37,107 +42,170 @@ sub import {
     warnings->import;
 }
 
-sub psgify (&) {
-    my $code = shift;
+sub new {
+    my $class = shift;
+    my $args = ref $_[0] ? shift : { @_ };
+
+    my $configloader = CloudForecast::ConfigLoader->new({
+        root_dir => $args->{root_dir},
+        global_config => $args->{global_config},
+        server_list => $args->{server_list},
+    });
+    $configloader->load_all();
+
+    $class->SUPER::new({
+        configloader => $configloader,
+        restarter => $args->{restarter},
+        port => $args->{port} || 5000,
+    });
+}
+
+sub run {
+    my $self = shift;
+
+    my $app = $self->build_app;
+    $app = builder {
+        enable 'Plack::Middleware::Lint';
+        enable 'Plack::Middleware::StackTrace';
+        $app;
+    };
+
+
+    my $loader = Plack::Loader->load(
+        'Starlet',
+        port => $self->port || 5000,
+        max_workers => 2,
+    );
+
+    my @watchdog_pid;
+    if ( $self->restarter ) {
+        CloudForecast::Log->debug("restarter start");
+        push @watchdog_pid, $self->configloader->watchdog;
+    }
+
+    $loader->run($app);
+
+    for my $pid ( @watchdog_pid ) {
+        kill 'TERM', $pid;
+        waitpid( $pid, 0 );
+    }
+}
+
+sub build_app {
+    my $self = shift;
     sub {
         my $env = shift;
-        my $req = Plack::Request->new($env);
-        my $p = delete $env->{'cloudforecast-web.args'};
-        my $res = $code->($req, $p);
-        my $res_t = ref $res || '';
-        if ( $res_t eq 'Plack::Response' ) {
-            return $res->finalize;
-        }
-        elsif ( $res_t eq 'ARRAY' ) {
-            return $res;
-        }
-        elsif ( !$res_t ) {
-            return [ 200, [ 'Content-Type' => 'text/html; charset=utf-8'], [ $res ] ];
-        }
-        else {
-            Carp::croak("unknown response type: $res, $res_t");
-        }
-    };
-}
-
-sub router_to_app {
-    my $router = shift;
-    sub {
-        if ( my $p = $router->match($_[0]) ) {
+        if ( my $p = $self->router->match($env) ) {
             my $code = delete $p->{action};
-            return [ 500, [], ['Internal Server Error'] ] unless $code; 
-            $_[0]->{'cloudforecast-web.args'} = $p;
-            return $code->(@_)
-        }
-        else {
-            return [ 404, [ 'Content-Type' => 'text/html; charset=utf-8' ], ['not found'] ];
-        }
-    }
-}
+            return $self->ise('uri match but no action found') unless $code;
 
-sub run_server {
-    my $runner = Plack::Runner->new;
-    $runner->parse_options(@_);
+            my $req = Plack::Request->new($env);
+            my $res = $code->($self, $req, $p);
 
-    my $router_app = router_to_app($_ROUTER);
-    my $app = sub {
-        local $KEY = refaddr $router_app;
-        $router_app->(@_);
+            my $res_t = ref $res || '';
+            if ( $res_t eq 'Plack::Response' ) {
+                return $res->finalize;
+            }
+            elsif ( $res_t eq 'ARRAY' ) {
+                return $res;
+            }
+            elsif ( !$res_t ) {
+                return $self->html_response( $res );
+            }
+            else {
+                Carp::croak("unknown response type: $res, $res_t");
+            }
+        }
+        return $self->not_found();
     };
-
-    $runner->run($app);
 }
 
-
-sub any($$;$) {
-    if ( @_ == 3 ) {
-        my ( $methods, $pattern, $code ) = @_;
-        $_ROUTER->connect(
-            $pattern,
-            { action => psgify { goto $code } },
-            { method => [ map { uc $_ } @$methods ] } 
-        );        
-    }
-    else {
-        my ( $pattern, $code ) = @_;
-        $_ROUTER->connect(
-            $pattern,
-            { action => psgify { goto $code } }
-        );
-    }
+sub ise {
+    my $self = shift;
+    my $error = shift;
+    CloudForecast::Log->warn($error) if $error;
+    $error ||= 'Internal Server Error';
+    return [ 500, [ 'Content-Type' => 'text/html; charset=utf-8' ], [$error] ];
 }
 
-sub get {
-    any( ['GET','HEAD'], $_[0], $_[1]  );
+sub not_found {
+    my $self = shift;
+    my $error = shift;
+    CloudForecast::Log->warn($error) if $error;
+    $error ||= 'Not Found';
+    return [ 404, [ 'Content-Type' => 'text/html; charset=utf-8' ], [$error] ];
 }
 
-sub post {
-    any( ['POST'], $_[0], $_[1]  );
-}
-
-sub get_data_section {
-    my $pkg = caller($DATA_SECTION_LEVEL);
-    my $data = $CACHE{$KEY}->{__data_section} ||= Data::Section::Simple->new($pkg)->get_data_section;
-    return @_ ? $data->{$_[0]} : $data;
+sub html_response {
+    my $self = shift;
+    my $message = shift;
+    return [ 200, [ 'Content-Type' => 'text/html; charset=utf-8'], [ $message ] ];
 }
 
 sub render {
-    my ( $key, @args ) = @_;
-    my $code = $CACHE{$KEY}->{$key} ||= do {
-        local $DATA_SECTION_LEVEL = $DATA_SECTION_LEVEL + 1;
-        my $tmpl = get_data_section($key);
+    my ( $self, $key, @args ) = @_;
+    my $code = do {
+        my $reader = Data::Section::Simple->new(ref $self);
+        my $tmpl = $reader->get_data_section($key);
         Carp::croak("unknown template file:$key") unless $tmpl;
-        Text::MicroTemplate->new(template => $tmpl, package_name => 'main')->code();
+        Text::MicroTemplate->new(template => $tmpl, package_name => ref($self) )->code();
     };
 
     package DB;
     local *DB::render = sub {
         my $coderef = (eval $code); ## no critic
         die "Cannot compile template '$key': $@" if $@;
-        $coderef->(@args);
+        my $html = $coderef->(@args);
+        $html = Encode::encode_utf8($html) if utf8::is_utf8($html);
+        $html;
     };
     goto &DB::render;
 }
+
+
+sub router {
+    my $class = shift;
+    my $router = $class->_ROUTER;
+    if ( !$router ) {
+        $router = $class->_ROUTER( Router::Simple->new() );
+    }
+    $router;
+}
+
+sub _any($$$;$) {
+    my $class = shift;
+    if ( @_ == 3 ) {
+        my ( $methods, $pattern, $code ) = @_;
+        $class->router->connect(
+            $pattern,
+            { action => $code },
+            { method => [ map { uc $_ } @$methods ] } 
+        );        
+    }
+    else {
+        my ( $pattern, $code ) = @_;
+        $class->router->connect(
+            $pattern,
+            { action => $code }
+        );
+    }
+}
+
+sub any {
+    my $class = caller;
+    $class->_any( @_ );
+}
+
+sub get {
+    my $class = caller;
+    $class->_any( ['GET','HEAD'], $_[0], $_[1]  );
+}
+
+sub post {
+    my $class = caller;
+    $class->_any( ['POST'], $_[0], $_[1]  );
+}
+
 
 1;
 
---
+++ lib/CloudForecast/Web/Server.pm
@@ -0,0 +1,163 @@
+package CloudForecast::Web::Server;
+
+use strict;
+use warnings;
+use CloudForecast::Web -base;
+use CloudForecast::Host;
+
+sub get_host {
+    my ( $self, $host ) = @_;
+    my $host_instance = CloudForecast::Host->new({
+        address => $host->{address},
+        hostname => $host->{hostname},
+        details => $host->{details},
+        resources => $host->{resources},
+        component_config => $host->{component_config},
+        global_config => $self->configloader->global_config,
+    });
+    $host_instance;
+}
+
+sub page_title {
+    my $self = shift;
+    my $page_title = $self->configloader->server_list_yaml;
+    $page_title =~ s!^(.+)/!!;
+    $page_title =~ s!\.[^.]+$!!;
+    $page_title;
+}
+
+# shortcut
+sub all_hosts {
+    my $self = shift;
+    $self->configloader->all_hosts;
+}
+
+sub server_list {
+    my $self = shift;
+    $self->configloader->server_list;
+}
+
+get '/' => sub {
+    my ( $self, $req, $p )  = @_;
+    return $self->render('index.mt');
+};
+
+get '/server' => sub {
+    my ($self, $req, $p ) = @_;
+
+    my $address = $req->param('address');
+    return $self->not_found('Address Not Found') unless $address;
+
+    my $host = $self->all_hosts->{$address};
+    return $self->not_found('Host Not Found') unless $host;
+
+    my $host_instance = $self->get_host($host);
+    my @graph_list = $host_instance->list_graph;
+
+    return $self->render('server.mt');
+};
+
+
+get '/graph' => sub {
+    my ($self, $req )  = @_;
+
+    my $address = $req->param('address');
+    return $self->not_found('Address Not Found') unless $address;
+    my $resource = $req->param('resource');
+    return $self->not_found('Resource Not Found') unless $resource;
+    my $key = $req->param('key');
+    return $self->not_found('Graph type key Not Found') unless $key;
+
+    my $span = $req->param('span') || 'd';
+    my $host = $self->all_hosts->{$address};
+    return $self->not_found('Host Not Found') unless $host;
+
+    my $host_instance = $self->get_host($host);
+    my ($img,$err) = $host_instance->draw_graph($resource,$key, $span);
+
+    return $self->ise($err) unless $img;
+    return [ 200, ['Content-Type','image/png'], [$img] ];
+};
+
+get '/default.css' => sub {
+    my ($self, $req) = @_;
+    return [ 200, ['Content-Type','text/css'], [ $self->render('css.mt')] ];
+}; 
+
+
+1;
+
+__DATA__
+@@ index.mt
+<html>
+<head>
+<title>CloudForecast Server List</title>
+<link rel="stylesheet" type="text/css" href="/default.css" />
+</head>
+<body>
+<h1 class="title">CloudForecast : <?= $self->page_title ?> </h1>
+
+<ul>
+<? my $i=0 ?>
+<? for my $server ( @{$self->server_list} ) { ?>
+<li><a href="#group-<?= $i ?>"><?= $server->{title} ?></a></li>
+<? $i++ } ?>
+</ul>
+
+<hr>
+
+<ul>
+<? my $k=0 ?>
+<? for my $server ( @{$self->server_list} ) { ?>
+<li id="group-<?= $k ?>"><?= $server->{title} ?></li>
+<ul>
+  <? for my $host ( @{$server->{hosts}} ) { ?>
+  <li><a href="/server?address=<?= $host->{address} ?>"><?= $host->{address} ?></a> <strong><?= $host->{hostname} ?></strong> <span class="details"><?= $host->{details} ?></a></li>
+  <? } ?>
+</ul>
+<? $k++ } ?>
+</ul>
+
+</body>
+</html>
+
+@@ server.mt
+<html>
+<head>
+<title>CloudForecast : <?= $self->page_title ?> : <?= $host->{address} ?></title>
+<link rel="stylesheet" type="text/css" href="/default.css" />
+</head>
+<body>
+<h1 class="title">CloudForecast : <?= $self->page_title ?></h1>
+<h2><span class="address"><?= $host->{address} ?></span> <strong><?= $host->{hostname} ?></strong> <span class="details"><?= $host->{details} ?></a></h2>
+
+<? for my $resource ( @graph_list ) { ?>
+<h4><?= $resource->{resource_class} ?></h4>
+<? for my $graph ( @{$resource->{graphs}} ) { ?>
+<nobr />
+<? for my $term ( qw/d w m y/ ) { ?>
+<img src="/graph?span=<?= $term ?>&amp;address=<?= $host->{address} ?>&amp;resource=<?= $resource->{resource} ?>&amp;key=<?= $graph ?>" />
+<? } ?>
+<br />
+<? } ?>
+<? } ?>
+
+</body>
+</html>
+
+
+@@ css.mt
+
+a { color: #5555cc;}
+a:link { color: #5555cc;}
+a:visited { color: #555599;}
+a:active { color: #999999;  }
+a:hover { color: #999999;  }
+
+ol, ul{
+    list-style-position:inside;
+}
+
+
+
+
