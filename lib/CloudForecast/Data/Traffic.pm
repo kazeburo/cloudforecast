4f1c1b1f7451140106ec179f22703a6928886ed8
Masahiro Nagano <kazeburo@gmail.com>
2012-02-17 12:35:32 +0900

    見切れているのでちょっと詰める

--- lib/CloudForecast/Data/Traffic.pm
+++ lib/CloudForecast/Data/Traffic.pm
@@ -58,13 +58,13 @@ DEF:ind=<%RRD%>:in:AVERAGE
 DEF:outd=<%RRD%>:out:AVERAGE
 CDEF:in=ind,0,125000000,LIMIT,8,*
 CDEF:out=outd,0,125000000,LIMIT,8,*
-AREA:in#00C000: Inbound   
-GPRINT:in:LAST:Current\:%6.2lf %sbps
-GPRINT:in:AVERAGE:Ave\:%6.2lf %sbps
-GPRINT:in:MAX:Max\:%6.2lf %sbps\c
-LINE1:out#0000FF: Outbound  
-GPRINT:out:LAST:Current\:%6.2lf %sbps
-GPRINT:out:AVERAGE:Ave\:%6.2lf %sbps
-GPRINT:out:MAX:Max\:%6.2lf %sbps\c
+AREA:in#00C000:Inbound  
+GPRINT:in:LAST:Current\:%6.2lf%sbps
+GPRINT:in:AVERAGE:Ave\:%6.2lf%sbps
+GPRINT:in:MAX:Max\:%6.2lf%sbps\c
+LINE1:out#0000FF:Outbound 
+GPRINT:out:LAST:Current\:%6.2lf%sbps
+GPRINT:out:AVERAGE:Ave\:%6.2lf%sbps
+GPRINT:out:MAX:Max\:%6.2lf%sbps\c
 
 
