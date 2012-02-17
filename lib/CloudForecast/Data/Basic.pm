522b9cfb390b270886ef9d3911c3aa3a92fbb57e
Masahiro Nagano <kazeburo@gmail.com>
2012-02-17 12:39:08 +0900

    もう少し詰める

--- lib/CloudForecast/Data/Basic.pm
+++ lib/CloudForecast/Data/Basic.pm
@@ -163,57 +163,57 @@ CDEF:my5=my5t,UN,0,my5t,IF
 # used
 CDEF:myelse=my2,my3,-,my7,-,my6,-,my5,-,1024,*,0,34359738368,LIMIT
 AREA:myelse#ffdd67:used      
-GPRINT:myelse:LAST:Cur\: %6.2lf%sByte
-GPRINT:myelse:AVERAGE:Ave\: %6.2lf%sByte
-GPRINT:myelse:MAX:Max\: %6.2lf%sByte\c
+GPRINT:myelse:LAST:Cur\:%6.2lf%sByte
+GPRINT:myelse:AVERAGE:Ave\:%6.2lf%sByte
+GPRINT:myelse:MAX:Max\:%6.2lf%sByte\c
 
 # shared
 CDEF:my55=my5,1024,*,0,34359738368,LIMIT
 STACK:my55#a6a6a6:shared    
-GPRINT:my55:LAST:Cur\: %6.2lf%sByte
-GPRINT:my55:AVERAGE:Ave\: %6.2lf%sByte
-GPRINT:my55:MAX:Max\: %6.2lf%sByte\c
+GPRINT:my55:LAST:Cur\:%6.2lf%sByte
+GPRINT:my55:AVERAGE:Ave\:%6.2lf%sByte
+GPRINT:my55:MAX:Max\:%6.2lf%sByte\c
 
 # buffer
 CDEF:my66=my6,1024,*,0,34359738368,LIMIT
 STACK:my66#8a8ae6:buffer    
-GPRINT:my66:LAST:Cur\: %6.2lf%sByte
-GPRINT:my66:AVERAGE:Ave\: %6.2lf%sByte
-GPRINT:my66:MAX:Max\: %6.2lf%sByte\c
+GPRINT:my66:LAST:Cur\:%6.2lf%sByte
+GPRINT:my66:AVERAGE:Ave\:%6.2lf%sByte
+GPRINT:my66:MAX:Max\:%6.2lf%sByte\c
 
 # cached
 CDEF:my77=my7,1024,*,0,34359738368,LIMIT
 STACK:my77#6060e0:cached    
 GPRINT:my77:LAST:Cur\: %6.2lf%sByte
-GPRINT:my77:AVERAGE:Ave\: %6.2lf%sByte
-GPRINT:my77:MAX:Max\: %6.2lf%sByte\c
+GPRINT:my77:AVERAGE:Ave\:%6.2lf%sByte
+GPRINT:my77:MAX:Max\:%6.2lf%sByte\c
 
 # avail real
 CDEF:my33=my3,1024,*,0,34359738368,LIMIT
 STACK:my33#80e080:avail real
-GPRINT:my33:LAST:Cur\: %6.2lf%sByte
-GPRINT:my33:AVERAGE:Ave\: %6.2lf%sByte
-GPRINT:my33:MAX:Max\: %6.2lf%sByte\c
+GPRINT:my33:LAST:Cur\:%6.2lf%sByte
+GPRINT:my33:AVERAGE:Ave\:%6.2lf%sByte
+GPRINT:my33:MAX:Max\:%6.2lf%sByte\c
 
 # total real
 CDEF:my222=my2,1024,*,0,34359738368,LIMIT
 LINE2:my222#000080:total real
-GPRINT:my222:LAST:Cur\: %6.2lf%sByte
-GPRINT:my222:AVERAGE:Ave\: %6.2lf%sByte
-GPRINT:my222:MAX:Max\: %6.2lf%sByte\c
+GPRINT:my222:LAST:Cur\:%6.2lf%sByte
+GPRINT:my222:AVERAGE:Ave\:%6.2lf%sByte
+GPRINT:my222:MAX:Max\:%6.2lf%sByte\c
 
 # used swap
 CDEF:my11=my1,my8,-,1024,*,0,34359738368,LIMIT
 LINE2:my11#ff6060:used  swap
-GPRINT:my11:LAST:Cur\: %6.2lf%sByte
-GPRINT:my11:AVERAGE:Ave\: %6.2lf%sByte
-GPRINT:my11:MAX:Max\: %6.2lf%sByte\c
+GPRINT:my11:LAST:Cur\:%6.2lf%sByte
+GPRINT:my11:AVERAGE:Ave\:%6.2lf%sByte
+GPRINT:my11:MAX:Max\:%6.2lf%sByte\c
 
 @@ tcpestab.def
 DEF:tcpestab=<%RRD%>:tcpestab:AVERAGE
 AREA:tcpestab#00C000: TCP Established   
-GPRINT:tcpestab:LAST:Cur\: %4.0lf
-GPRINT:tcpestab:AVERAGE:Ave\: %4.0lf
-GPRINT:tcpestab:MAX:Max\: %4.0lf
-GPRINT:tcpestab:MIN:Min\: %4.0lf\c
+GPRINT:tcpestab:LAST:Cur\:%4.0lf
+GPRINT:tcpestab:AVERAGE:Ave\:%4.0lf
+GPRINT:tcpestab:MAX:Max\:%4.0lf
+GPRINT:tcpestab:MIN:Min\:%4.0lf\c
 
--- lib/CloudForecast/Data/Traffic.pm
+++ lib/CloudForecast/Data/Traffic.pm
@@ -59,11 +59,11 @@ DEF:outd=<%RRD%>:out:AVERAGE
 CDEF:in=ind,0,125000000,LIMIT,8,*
 CDEF:out=outd,0,125000000,LIMIT,8,*
 AREA:in#00C000:Inbound  
-GPRINT:in:LAST:Current\:%6.2lf%sbps
+GPRINT:in:LAST:Cur\:%6.2lf%sbps
 GPRINT:in:AVERAGE:Ave\:%6.2lf%sbps
 GPRINT:in:MAX:Max\:%6.2lf%sbps\c
 LINE1:out#0000FF:Outbound 
-GPRINT:out:LAST:Current\:%6.2lf%sbps
+GPRINT:out:LAST:Cur\:%6.2lf%sbps
 GPRINT:out:AVERAGE:Ave\:%6.2lf%sbps
 GPRINT:out:MAX:Max\:%6.2lf%sbps\c
 
