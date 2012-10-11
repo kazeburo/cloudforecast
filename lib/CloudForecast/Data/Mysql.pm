f45ea41d3596496dd349b2e7b5fccbd64dfe0516
Masahiro Nagano <kazeburo@gmail.com>
2012-10-11 16:58:03 +0900

    rename slow_query_log

--- lib/CloudForecast/Data/Mysql.pm
+++ lib/CloudForecast/Data/Mysql.pm
@@ -33,7 +33,7 @@ sysinfo {
         }
 
         map { push @sysinfo, $_, $sysinfo->{$_} } grep { exists $sysinfo->{$_} } 
-            qw/max_connections max_connect_errors thread_cache_size slow_query_log log_slow_queries long_query_time log_queries_not_using_indexes/;
+            qw/max_connections max_connect_errors thread_cache_size slow_query_log long_query_time log_queries_not_using_indexes/;
         
     }
     return \@sysinfo;
@@ -60,6 +60,7 @@ fetcher {
     $sysinfo{uptime} = $status{Uptime} || 0;
     map { $sysinfo{$_} = $variable{$_} } grep { exists $variable{$_} }
         qw/version version_comment slow_query_log log_slow_queries long_query_time log_queries_not_using_indexes max_connections max_connect_errors thread_cache_size/;
+    delete $sysinfo{log_slow_queries} if exists $sysinfo{log_slow_queries} && exists $sysinfo{slow_query_log};
     $c->ledge_set('sysinfo', \%sysinfo );
 
     return [ map { $status{$_} } qw/Com_delete Com_insert Com_replace Com_select Com_update Slow_queries
